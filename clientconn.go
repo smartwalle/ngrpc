5343527781fdf5456071086a67b2f07cae51d944
smartwalle <smartwalle@gmail.com>
2021-04-12 11:38:57 +0800

    header

--- clientconn.go
+++ clientconn.go
@@ -2,52 +2,90 @@ package grpc4go
 
 import (
 	"context"
+	"errors"
 	"github.com/smartwalle/pool4go"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+)
+
+var (
+	ErrServerNotFound = errors.New("server not found")
 )
 
 type ClientConn struct {
-	target string
-	opts   []grpc.DialOption
-	pool   pool4go.Pool
+	target  string
+	pool    pool4go.Pool
+	retries int
 }
 
-func NewClientConn(target string, poolSize int, opts ...grpc.DialOption) *ClientConn {
+func Dial(target string, poolSize int, opts ...grpc.DialOption) *ClientConn {
 	if poolSize <= 0 {
 		poolSize = 1
 	}
 	var c = &ClientConn{}
 	c.target = target
-	c.opts = opts
 	c.pool = pool4go.New(func() (pool4go.Conn, error) {
 		return grpc.Dial(target, opts...)
 	}, pool4go.WithMaxIdle(poolSize), pool4go.WithMaxOpen(poolSize))
+	c.retries = poolSize
+	return c
+}
+
+func DialContext(ctx context.Context, target string, poolSize int, opts ...grpc.DialOption) *ClientConn {
+	if poolSize <= 0 {
+		poolSize = 1
+	}
+	var c = &ClientConn{}
+	c.target = target
+	c.pool = pool4go.New(func() (pool4go.Conn, error) {
+		return grpc.DialContext(ctx, target, opts...)
+	}, pool4go.WithMaxIdle(poolSize), pool4go.WithMaxOpen(poolSize))
+	c.retries = poolSize
 	return c
 }
 
 func (this *ClientConn) Invoke(ctx context.Context, method string, args, reply interface{}, opts ...grpc.CallOption) error {
-	var conn, err = this.pool.Get()
-	if err != nil {
+	for i := 0; i <= this.retries; i++ {
+		var conn, err = this.pool.Get()
+		if err != nil {
+			return err
+		}
+
+		var nConn = conn.(*grpc.ClientConn)
+
+		var state = nConn.GetState()
+		if state == connectivity.TransientFailure || state == connectivity.Shutdown {
+			this.pool.Release(conn)
+			continue
+		}
+
+		err = nConn.Invoke(ctx, method, args, reply, opts...)
+		this.pool.Put(conn)
 		return err
 	}
-
-	var nConn = conn.(*grpc.ClientConn)
-	err = nConn.Invoke(ctx, method, args, reply, opts...)
-	this.pool.Put(conn)
-	return err
+	return ErrServerNotFound
 }
 
-// NewStream 目前还不能直接使用
 func (this *ClientConn) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
-	var conn, err = this.pool.Get()
-	if err != nil {
-		return nil, err
-	}
+	for i := 0; i <= this.retries; i++ {
+		var conn, err = this.pool.Get()
+		if err != nil {
+			return nil, err
+		}
 
-	var nConn = conn.(*grpc.ClientConn)
-	stream, err := nConn.NewStream(ctx, desc, method, opts...)
-	this.pool.Put(conn)
-	return stream, err
+		var nConn = conn.(*grpc.ClientConn)
+
+		var state = nConn.GetState()
+		if state == connectivity.TransientFailure || state == connectivity.Shutdown {
+			this.pool.Release(conn)
+			continue
+		}
+
+		stream, err := nConn.NewStream(ctx, desc, method, opts...)
+		this.pool.Put(conn)
+		return stream, err
+	}
+	return nil, ErrServerNotFound
 }
 
 func (this *ClientConn) Close() {
