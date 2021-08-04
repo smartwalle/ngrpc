08983d05a0e0b57e00d4928798e378987bb703d7
smartwalle <smartwalle@gmail.com>
2021-08-04 17:15:14 +0800

    Unary Call Timeout

---
+++ middleware/timeout/option.go
@@ -0,0 +1,48 @@
+package timeout
+
+import (
+	"google.golang.org/grpc"
+	"time"
+)
+
+type CallOption struct {
+	grpc.EmptyCallOption
+	apply func(*option)
+}
+
+type option struct {
+	timeout time.Duration
+}
+
+func Disable() CallOption {
+	return WithTimeout(0)
+}
+
+func WithTimeout(timeout time.Duration) CallOption {
+	return CallOption{apply: func(opt *option) {
+		opt.timeout = timeout
+	}}
+}
+
+func mergeOptions(opt *option, callOptions []CallOption) *option {
+	if len(callOptions) == 0 {
+		return opt
+	}
+	var nOpt = &option{}
+	*nOpt = *opt
+	for _, f := range callOptions {
+		f.apply(nOpt)
+	}
+	return nOpt
+}
+
+func filterOptions(inOpts []grpc.CallOption) (grpcOptions []grpc.CallOption, retryOptions []CallOption) {
+	for _, inOpt := range inOpts {
+		if opt, ok := inOpt.(CallOption); ok {
+			retryOptions = append(retryOptions, opt)
+		} else {
+			grpcOptions = append(grpcOptions, inOpt)
+		}
+	}
+	return grpcOptions, retryOptions
+}
---
+++ middleware/timeout/unary.go
@@ -0,0 +1,43 @@
+package timeout
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"time"
+)
+
+func WithUnaryCallTimeout(opts ...CallOption) grpc.DialOption {
+	var defaultOption = &option{
+		timeout: 5 * time.Second,
+	}
+	defaultOption = mergeOptions(defaultOption, opts)
+	return grpc.WithUnaryInterceptor(unaryClientTimeout(defaultOption))
+}
+
+func unaryClientTimeout(defaultOption *option) grpc.UnaryClientInterceptor {
+	return func(pCtx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		var grpcOpts, retryOpts = filterOptions(opts)
+		var callOption = mergeOptions(defaultOption, retryOpts)
+
+		var err error
+
+		var nCtx, cancel = callContext(pCtx, callOption)
+		defer func() {
+			if cancel != nil {
+				cancel()
+			}
+		}()
+		err = invoker(nCtx, method, req, reply, cc, grpcOpts...)
+
+		return err
+	}
+}
+
+func callContext(pCtx context.Context, callOption *option) (context.Context, context.CancelFunc) {
+	var nCtx = pCtx
+	var cancel context.CancelFunc
+	if callOption.timeout > 0 {
+		nCtx, cancel = context.WithTimeout(nCtx, callOption.timeout)
+	}
+	return nCtx, cancel
+}
