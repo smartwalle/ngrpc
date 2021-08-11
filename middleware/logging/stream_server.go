db8db4b87e3b5d032ff428dc54276e02e4c1b926
smartwalle <smartwalle@gmail.com>
2021-08-11 15:23:34 +0800

    日志

--- middleware/logging/stream_server.go
+++ middleware/logging/stream_server.go
@@ -1,34 +1,10 @@
 package logging
 
 import (
-	"context"
 	"google.golang.org/grpc"
 	"io"
 	"time"
 )
-
-// WithStreamClient 客户端流调用日志处理
-func WithStreamClient(opts ...Option) grpc.DialOption {
-	var defaultOption = &option{
-		logger: &nilLogger{},
-	}
-	defaultOption = mergeOptions(defaultOption, opts)
-	return grpc.WithChainStreamInterceptor(streamClientLog(defaultOption))
-}
-
-func streamClientLog(defaultOption *option) grpc.StreamClientInterceptor {
-	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
-		defaultOption.logger.Printf("GRPC 请求建立到 [%s - %s] 的流 \n", cc.Target(), method)
-
-		var nStream, err = streamer(ctx, desc, cc, method, opts...)
-		if err != nil {
-			defaultOption.logger.Printf("GRPC 建立到 [%s - %s] 的流发生错误 [%v] \n", cc.Target(), method, err)
-		} else {
-			defaultOption.logger.Printf("GRPC 建立到 [%s - %s] 的流成功 \n", cc.Target(), method)
-		}
-		return nStream, err
-	}
-}
 
 // WithStreamServer 服务端流调用日志处理
 func WithStreamServer(opts ...Option) grpc.ServerOption {
--- middleware/logging/unary_server.go
+++ middleware/logging/unary_server.go
@@ -6,31 +6,6 @@ import (
 	"time"
 )
 
-// WithUnaryClient 客户端普通方法调用日志处理
-func WithUnaryClient(opts ...Option) grpc.DialOption {
-	var defaultOption = &option{
-		logger: &nilLogger{},
-	}
-	defaultOption = mergeOptions(defaultOption, opts)
-	return grpc.WithChainUnaryInterceptor(unaryClientLog(defaultOption))
-}
-
-func unaryClientLog(defaultOption *option) grpc.UnaryClientInterceptor {
-	return func(pCtx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		defaultOption.logger.Printf("GRPC 请求访问 [%s - %s] 接口, 请求参数 [%v] \n", cc.Target(), method, req)
-
-		var start = time.Now()
-		var err = invoker(pCtx, method, req, reply, cc, opts...)
-		var end = time.Now()
-		if err != nil {
-			defaultOption.logger.Printf("GRPC 接口 [%s - %s] 返回异常, 持续时间 [%v], 返回数据 [%v], 错误信息 [%v] \n", cc.Target(), method, end.Sub(start), reply, err)
-		} else {
-			defaultOption.logger.Printf("GRPC 接口 [%s - %s] 返回成功, 持续时间 [%v], 返回数据 [%v]\n", cc.Target(), method, end.Sub(start), reply)
-		}
-		return err
-	}
-}
-
 // WithUnaryServer 服务端普通方法调用日志处理
 func WithUnaryServer(opts ...Option) grpc.ServerOption {
 	var defaultOption = &option{
