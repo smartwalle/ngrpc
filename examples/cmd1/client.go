3ea7eb85545c5b091642f93e3471682d6003536c
SmartWalle <smartwalle@gmail.com>
2024-03-21 16:11:40 +0800

    demo

--- examples/cmd1/client.go
+++ examples/cmd1/client.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/smartwalle/ngrpc"
 	"github.com/smartwalle/ngrpc/examples"
 	"github.com/smartwalle/ngrpc/examples/proto"
 	"github.com/smartwalle/ngrpc/registry/etcd"
+	"google.golang.org/grpc"
 	"log"
 	"time"
 )
@@ -13,7 +15,12 @@ import (
 func main() {
 	var r = etcd.NewRegistry(examples.GetETCDClient())
 
-	var conn = ngrpc.Dial(r.BuildPath("grpc1", "hello", "cmd1"), ngrpc.WithInsecure(), ngrpc.WithTimeout(time.Second*3))
+	var conn = ngrpc.Dial(
+		r.BuildPath("grpc1", "hello", "cmd1"),
+		ngrpc.WithInsecure(),
+		ngrpc.WithTimeout(time.Second*3),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "round_robin"}`)),
+	)
 
 	var client = proto.NewHelloWorldClient(conn)
 
